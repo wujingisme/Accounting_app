b9d0d7d249998f8313f1c6813b4c6c5db2696d8b
wujingisme <1472420872@qq.com>
2019-04-13 22:43:11 +0800

    4.13

---
+++ app/src/main/java/com/example/kechennsheji/AnalysisIncomeActivity.java
@@ -0,0 +1,293 @@
+package com.example.kechennsheji;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.kechennsheji.SQLite.DatabaseHelper;
+import com.example.kechennsheji.SQLite.DatabaseHelperPayin;
+import com.example.kechennsheji.User.UserActivity;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class AnalysisIncomeActivity extends AppCompatActivity {
+    private DatabaseHelperPayin helperPayin;
+    private Button jizhang,chakan,user,fenxibaogao;
+    private Button bt_shouru,bt_huafei;
+
+    private EditText editText1, editText2;
+    private TextView tv_gongzhi,tv_zhuanzhuang,tv_jiangjin,tv_touzhi,tv_qita,tv_income;
+    private PieChart mChart;
+    private final String TAG="Piechart";
+    private String[] x = new String[] { "工资", "转账", "奖金","投资","其他" };
+    private float m_gongzhi,m_zhuanzhuang,m_jiangjin,m_touzhi,m_qita,total;
+    private float m_gongzhi2,m_zhuanzhuang2,m_jiangjin2,m_touzhi2,m_qita2;
+
+    // 凑成100 % 100
+    private float[] y ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis_income);
+        mChart = (PieChart) findViewById(R.id.pie_chart);
+        // 图的描述
+        mChart.setCenterText("饼状图");
+        // 以7个对应数据集做测试
+        QTotal_income();
+        initview2();
+         total=m_gongzhi+m_zhuanzhuang+m_jiangjin+m_touzhi+m_qita;
+        m_gongzhi=((float)m_gongzhi/total)*100;
+        m_zhuanzhuang=((float)m_zhuanzhuang/total)*100;
+        m_jiangjin=((float)m_jiangjin/total)*100;
+        m_touzhi=((float)m_touzhi/total)*100;
+        m_qita=((float)m_qita/total)*100;
+
+        y = new float[]{m_gongzhi,m_zhuanzhuang,m_jiangjin,m_touzhi,m_qita};
+       setData(x.length);
+
+    }
+
+    DecimalFormat df=new DecimalFormat("0.00");
+ public void QTotal_income()
+    {
+        helperPayin=new DatabaseHelperPayin(AnalysisIncomeActivity.this,"table_payin",null,1);
+        SQLiteDatabase db=helperPayin.getReadableDatabase();
+
+       Cursor cursor_gongzhi=db.rawQuery("select sum(money) from table_payin where sort=?",new String []{"工资"});
+        if (cursor_gongzhi.moveToFirst())
+        { do
+        { m_gongzhi=cursor_gongzhi.getInt(0); }
+        while (cursor_gongzhi.moveToNext());
+        }
+        m_gongzhi2=m_gongzhi;
+        cursor_gongzhi.close();
+     // Log.d(TAG,String.valueOf(m_gongzhi));
+         Cursor cursor_zhuanzhuang=db.rawQuery("select sum(money) from table_payin where sort=?",new String []{"转账"});
+        if (cursor_zhuanzhuang.moveToFirst())
+        { do
+        {
+            m_zhuanzhuang=cursor_zhuanzhuang.getInt(0); }
+        while (cursor_zhuanzhuang.moveToNext());
+        }m_zhuanzhuang2=m_zhuanzhuang;
+        cursor_zhuanzhuang.close();
+       // Log.d(TAG,String.valueOf(m_zhuanzhuang));
+       Cursor cursor_jiangjin=db.rawQuery("select sum(money) from table_payin where sort=?",new String []{"奖金"});
+
+        if(cursor_jiangjin.moveToFirst())
+        {
+            do{
+                m_jiangjin=cursor_jiangjin.getInt(0);
+            }while (cursor_jiangjin.moveToNext());
+        }m_jiangjin2=m_jiangjin;
+        cursor_jiangjin.close();
+      //  Log.d(TAG,String.valueOf(m_jiangjin));
+
+        Cursor cursor_touzhi=db.rawQuery("select sum(money) from table_payin where sort=?",new String []{"投资"});
+        if(cursor_touzhi.moveToFirst())
+        {
+            do {
+                m_touzhi=cursor_touzhi.getInt(0);
+
+            }while (cursor_touzhi.moveToNext()) ;
+        }m_touzhi2=m_touzhi;
+        cursor_touzhi.close();
+       // Log.d(TAG,String.valueOf(m_touzhi));
+       Cursor cursor_qita=db.rawQuery("select sum(money) from table_payin where sort=?",new String []{"其他"});
+        if(cursor_qita.moveToFirst())
+        {
+            do {
+                m_qita=cursor_qita.getInt(0);
+
+            }while (cursor_qita.moveToNext()) ;
+        }m_qita2=m_qita;
+        cursor_qita.close();
+        Log.d(TAG,String.valueOf(m_qita)+"  qita");
+
+    }
+
+    public void initview2()
+    {
+        tv_income=findViewById(R.id.total_income2);
+        tv_gongzhi=findViewById(R.id.total_gongzhi2);
+        tv_jiangjin=findViewById(R.id.total_jiangjin2);
+        tv_touzhi=findViewById(R.id.total_touzhi2);
+        tv_zhuanzhuang=findViewById(R.id.total_zhuanzhuang2);
+        tv_qita=findViewById(R.id.total_qita2);
+
+        tv_income.setText(String.valueOf(m_gongzhi+m_zhuanzhuang+m_jiangjin+m_touzhi+m_qita)+" 元");
+        tv_gongzhi.setText(String.valueOf(m_gongzhi)+" 元");
+        tv_zhuanzhuang.setText(String.valueOf((m_zhuanzhuang))+" 元");
+        tv_jiangjin.setText(String.valueOf(m_jiangjin)+" 元");
+        tv_touzhi.setText((String.valueOf(m_touzhi))+" 元");
+        tv_qita.setText(String .valueOf(m_qita)+" 元");
+    }
+
+    private void setData(int count) {
+
+        // 准备x"轴"数据：在i的位置，显示x[i]字符串
+        ArrayList<String> xVals = new ArrayList<String>();
+        // 真实的饼状图百分比分区。
+        // Entry包含两个重要数据内容：position和该position的数值。
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+
+        for (int xi = 0; xi < count; xi++) {
+            xVals.add(xi, x[xi]);
+            // y[i]代表在x轴的i位置真实的百分比占
+            yVals.add(new Entry(y[xi], xi));
+        }
+        PieDataSet yDataSet = new PieDataSet(yVals, "百分比占");
+        // 每个百分比占区块绘制的不同颜色
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.BLUE);
+        colors.add(Color.YELLOW);
+        colors.add(Color.GRAY);
+        yDataSet.setColors(colors);
+        // 将x轴和y轴设置给PieData作为数据源
+        PieData data = new PieData(xVals, yDataSet);
+        // 设置成PercentFormatter将追加%号
+        data.setValueFormatter(new PercentFormatter());
+        // 文字的颜色
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTextSize(10f);
+        // 最终将全部完整的数据喂给PieChart
+        mChart.setData(data);
+        mChart.invalidate();
+
+        jizhang = findViewById(R.id.btn_jizhang);
+        jizhang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnalysisIncomeActivity.this, MainPayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+        chakan = findViewById(R.id.btn_chakanzhangdan);
+        chakan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnalysisIncomeActivity.this, BillListActivity.class);
+                startActivity(intent);
+            }
+        });
+        fenxibaogao = findViewById(R.id.btn_fenxibaogao);
+        fenxibaogao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnalysisIncomeActivity.this, AnalysisChartActivity.class);
+                startActivity(intent);
+            }
+        });
+        user = findViewById(R.id.btn_yonghuzhongxin);
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnalysisIncomeActivity.this,UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        bt_huafei=findViewById(R.id.bt_huafei);
+        bt_huafei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AnalysisIncomeActivity.this,AnalysisChartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+     /*  //调出日历
+        editText1 = findViewById(R.id.Et_date1);
+        editText2 = findViewById(R.id.Et_date2);
+        editText1.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    showDatePickDlg();
+                    return true;
+                }
+                return false;
+            }
+        });
+        editText2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    showDatePickDlg1();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        editText1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showDatePickDlg();
+                }
+            }
+        });
+        editText2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showDatePickDlg1();
+                }
+            }
+        });*/
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    protected void showDatePickDlg() {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(AnalysisIncomeActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                AnalysisIncomeActivity.this.editText1.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+
+    }
+    protected void showDatePickDlg1() {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog1 = new DatePickerDialog(AnalysisIncomeActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                AnalysisIncomeActivity.this.editText2.setText(year + "-" + monthOfYear + "-" + dayOfMonth);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog1.show();
+
+
+    }
+}
--- app/src/main/java/com/example/kechennsheji/BillListActivity.java
+++ app/src/main/java/com/example/kechennsheji/BillListActivity.java
@@ -1,26 +1,23 @@
 package com.example.kechennsheji;
 
-import android.annotation.SuppressLint;
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.kechennsheji.SQLite.DatabaseHelper;
 import com.example.kechennsheji.SQLite.DatabaseHelperPayin;
 import com.example.kechennsheji.User.UserActivity;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,24 +32,33 @@ public class BillListActivity extends AppCompatActivity {
     private EditText editText1, editText2;
     private ListView ls;
     private Boolean flag=true;
+    private Boolean bolean=true;
+    private TextView expense,income,left;
+    private TextView tv_sort,tv_money,tv_date;
+    int sum_income,sum_expense,sum_left=0;
+    private static  final String TAG="test";
  private List<Map<String,Object>>datalist=new ArrayList<Map<String,Object>>();
  private int[] itemIdArr=new int[]{R.id.tv_shouzhi,id.tv_sort,id.tv_jiner,id.tv_riqi};
  private String[]   dataKeyArr=new String[]{"money","datetime","sort","introduce"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_billlist);
-        zhichu_shouru=findViewById(id.zhichuorsouru);
+        initview();
+        QTotal_expense();
+        QTotal_income();
+        QTotal_left();
         zhichu_shouru.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(flag==true)
-                {    datalist.clear();
+                {       datalist.clear();
                     helper=new DatabaseHelperPayin(BillListActivity.this,"table_payin",null,1);
-                   SQLiteDatabase db=helper.getReadableDatabase();
-
-                    Cursor cursor = db.query("table_payin", null, null, null, null, null, null);
+                    SQLiteDatabase db=helper.getReadableDatabase();
+                    Cursor cursor;
+                    cursor = db.query("table_payin", null, null, null, null,null, null);
                     while (cursor.moveToNext()) {
                         int money = cursor.getInt(0);
                         String datetime = cursor.getString(1);
@@ -70,16 +76,20 @@ public class BillListActivity extends AppCompatActivity {
                         map.put("introduce",introduce);
                         datalist.add(map);
                     }
-                    zhichu_shouru.setText("支出");
+                    tv_money_income_Onclick();
+                    zhichu_shouru.setText("当前为收入");
                     flag=false;
+                    income.setText(":"+String.valueOf(sum_income)+"元");
 
                 }
                 else{
+
+                   // Log.d(TAG, String.valueOf(sum));
                     datalist.clear();
                     helper_out=new DatabaseHelper(BillListActivity.this,"table_payout",null,1);
                    SQLiteDatabase db=helper_out.getReadableDatabase();
-
-                    Cursor cursor = db.query("table_payout", null, null, null, null, null, null);
+                    Cursor cursor;
+                   cursor = db.query("table_payout", null, null, null, null, null, "money desc");
                     while (cursor.moveToNext()) {
                         int money = cursor.getInt(0);
                         String datetime = cursor.getString(1);
@@ -97,15 +107,16 @@ public class BillListActivity extends AppCompatActivity {
                         map.put("introduce",introduce);
                         datalist.add(map);
                     }
-                    zhichu_shouru.setText("收入");
+                    tv_money_expense_Onclick();
+                    zhichu_shouru.setText("当前为支出");
                     flag=true;
+
+                    expense.setText(":"+String.valueOf(sum_expense)+"元");
+
                 }
 
             }
         });
-
-
-
 
 
         jizhang=findViewById(id.btn_jizhang);
@@ -187,6 +198,183 @@ public class BillListActivity extends AppCompatActivity {
             }
         });*/
     }
+    public  void   initview() {
+        zhichu_shouru=findViewById(id.zhichuorsouru);
+        expense=findViewById(R.id.total_expense2);
+        income=findViewById(id.total_income2);
+        left=findViewById(id.the_left2);
+        tv_money=findViewById(R.id._shouzhi);
+        tv_sort=findViewById(id._sort);
+        tv_date=findViewById(id._jiner);
+
+    }
+
+      public void QTotal_expense()
+    {
+        helper_out=new DatabaseHelper(BillListActivity.this,"table_payout",null,1);
+        SQLiteDatabase db=helper_out.getReadableDatabase();
+        //Cursor cursor=db.rawQuery("select sum(money) from table_payout",null);
+        Cursor cursor=db.rawQuery("select sum(money) from table_payout where sort=?",new String []{"交通"});
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                sum_expense=cursor.getInt(0);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+
+    }
+    public  void QTotal_income()
+    {
+        helper=new DatabaseHelperPayin(BillListActivity.this,"table_payin",null,1);
+        SQLiteDatabase db=helper.getReadableDatabase();
+        Cursor cursor=db.rawQuery("select sum(money) from table_payin",null);
+
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                sum_income=cursor.getInt(0);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+    }
+    public  void  QTotal_left(){
+        sum_left=sum_income-sum_expense;
+        left.setText(String.valueOf(sum_left));
+    }
+    public void tv_money_expense_Onclick()
+    {
+        tv_money.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag==true)
+                {
+                    datalist.clear();
+                    helper_out=new DatabaseHelper(BillListActivity.this,"table_payout",null,1);
+                    SQLiteDatabase db=helper_out.getReadableDatabase();
+                    Cursor cursor;
+                    cursor = db.query("table_payout", null, null, null, null,null, "money desc");
+                    while (cursor.moveToNext()) {
+                        int money = cursor.getInt(0);
+                        String datetime = cursor.getString(1);
+                        String sort = cursor.getString(2);
+                        String introduce= cursor.getString(3);
+
+                        ls=findViewById(R.id.List_1);
+                        SimpleAdapter simpleAdapter=new SimpleAdapter(BillListActivity.this,datalist,R.layout.item_list,dataKeyArr,itemIdArr);
+                        ls.setAdapter(simpleAdapter);
+                        Map<String,Object> map;
+                        map=new HashMap<String,Object>();
+                        map.put("money",money);
+                        map.put("datetime",datetime);
+                        map.put("sort",sort);
+                        map.put("introduce",introduce);
+                        datalist.add(map);
+                    }
+                    flag=false;
+                    tv_money.setText("金额(desc)");
+                }
+                else
+                {
+
+                    datalist.clear();
+                    helper_out=new DatabaseHelper(BillListActivity.this,"table_payout",null,1);
+                    SQLiteDatabase db=helper_out.getReadableDatabase();
+                    Cursor cursor;
+                    cursor = db.query("table_payout", null, null, null, null,null, "money asc");
+                    while (cursor.moveToNext()) {
+                        int money = cursor.getInt(0);
+                        String datetime = cursor.getString(1);
+                        String sort = cursor.getString(2);
+                        String introduce= cursor.getString(3);
+
+                        ls=findViewById(R.id.List_1);
+                        SimpleAdapter simpleAdapter=new SimpleAdapter(BillListActivity.this,datalist,R.layout.item_list,dataKeyArr,itemIdArr);
+                        ls.setAdapter(simpleAdapter);
+                        Map<String,Object> map;
+                        map=new HashMap<String,Object>();
+                        map.put("money",money);
+                        map.put("datetime",datetime);
+                        map.put("sort",sort);
+                        map.put("introduce",introduce);
+                        datalist.add(map);
+                    }
+                    flag=true;
+                    tv_money.setText("金额(asc)");
+                }
+            }
+        });
+
+    }
+
+    public void tv_money_income_Onclick()
+    {
+        tv_money.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag==true)
+                {
+                    datalist.clear();
+                    helper=new DatabaseHelperPayin(BillListActivity.this,"table_payin",null,1);
+                    SQLiteDatabase db=helper.getReadableDatabase();
+                    Cursor cursor;
+                    cursor = db.query("table_payin", null, null, null, null,null, "money desc");
+                    while (cursor.moveToNext()) {
+                        int money = cursor.getInt(0);
+                        String datetime = cursor.getString(1);
+                        String sort = cursor.getString(2);
+                        String introduce= cursor.getString(3);
+
+                        ls=findViewById(R.id.List_1);
+                        SimpleAdapter simpleAdapter=new SimpleAdapter(BillListActivity.this,datalist,R.layout.item_list,dataKeyArr,itemIdArr);
+                        ls.setAdapter(simpleAdapter);
+                        Map<String,Object> map;
+                        map=new HashMap<String,Object>();
+                        map.put("money",money);
+                        map.put("datetime",datetime);
+                        map.put("sort",sort);
+                        map.put("introduce",introduce);
+                        datalist.add(map);
+                    }
+                    flag=false;
+                    tv_money.setText("金额(desc)");
+                }
+                else
+                {
+
+                    datalist.clear();
+                    helper=new DatabaseHelperPayin(BillListActivity.this,"table_payin",null,1);
+                    SQLiteDatabase db=helper.getReadableDatabase();
+                    Cursor cursor;
+                    cursor = db.query("table_payin", null, null, null, null,null, "money asc");
+                    while (cursor.moveToNext()) {
+                        int money = cursor.getInt(0);
+                        String datetime = cursor.getString(1);
+                        String sort = cursor.getString(2);
+                        String introduce= cursor.getString(3);
+
+                        ls=findViewById(R.id.List_1);
+                        SimpleAdapter simpleAdapter=new SimpleAdapter(BillListActivity.this,datalist,R.layout.item_list,dataKeyArr,itemIdArr);
+                        ls.setAdapter(simpleAdapter);
+                        Map<String,Object> map;
+                        map=new HashMap<String,Object>();
+                        map.put("money",money);
+                        map.put("datetime",datetime);
+                        map.put("sort",sort);
+                        map.put("introduce",introduce);
+                        datalist.add(map);
+                    }
+                    flag=true;
+                    tv_money.setText("金额(asc)");
+                }
+            }
+        });
+
+    }
+
 
   //  @SuppressLint("ClickableViewAccessibility")
  /*   protected void showDatePickDlg() {
